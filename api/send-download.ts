7f66e2f9efa556be824cdc6a0eac873e4c44b405
amberwoodhousepress <dream@amberwoodhousepress.com>
2026-02-01 18:42:19 -0500

    Update send-download.ts

--- api/send-download.ts
+++ api/send-download.ts
@@ -25,7 +25,143 @@ export default async function handler(
   to: email,
   subject: "Your Activity Pack Is Ready",
   template_id: "5c8d9c8a-bb98-4f4f-917b-bb420ffcec0e",
-  html: "<!-- required by sdk -->"
+  html: <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html dir="ltr" lang="en">
+  <head>
+    <meta content="width=device-width" name="viewport" />
+    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
+    <meta name="x-apple-disable-message-reformatting" />
+    <meta content="IE=edge" http-equiv="X-UA-Compatible" />
+    <meta name="x-apple-disable-message-reformatting" />
+    <meta
+      content="telephone=no,address=no,email=no,date=no,url=no"
+      name="format-detection" />
+  </head>
+  <body>
+    <!--$--><!--html--><!--head-->
+    <div
+      style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0"
+      data-skip-in-text="true">
+      ✨ Your Activity Pack is Ready ✨
+      <div>
+         ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿
+      </div>
+    </div>
+    <!--body-->
+    <table
+      border="0"
+      width="100%"
+      cellpadding="0"
+      cellspacing="0"
+      role="presentation"
+      align="center">
+      <tbody>
+        <tr>
+          <td>
+            <table
+              align="center"
+              width="100%"
+              border="0"
+              cellpadding="0"
+              cellspacing="0"
+              role="presentation"
+              style="font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;font-size:1.0769230769230769em;min-height:100%;line-height:155%">
+              <tbody>
+                <tr>
+                  <td>
+                    <table
+                      align="left"
+                      width="100%"
+                      border="0"
+                      cellpadding="0"
+                      cellspacing="0"
+                      role="presentation"
+                      style="align:left;width:100%;padding-left:0px;padding-right:0px;line-height:155%;max-width:600px;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif">
+                      <tbody>
+                        <tr>
+                          <td>
+                            <p
+                              style="margin:0;padding:0;font-size:1em;padding-top:0.5em;padding-bottom:0.5em">
+                              <span><strong>Hello,</strong></span>
+                            </p>
+                            <p
+                              class="node-paragraph"
+                              style="margin:0;padding:0;font-size:1em;padding-top:0.5em;padding-bottom:0.5em">
+                              <span>We’re so happy you’re here 🤍</span
+                              ><br /><span
+                                >As promised, here’s your free printable
+                                activity pack, created with intention,
+                                imagination, and gentle learning in mind.</span
+                              >
+                            </p>
+                            <p
+                              class="node-paragraph"
+                              style="margin:0;padding:0;font-size:1em;padding-top:0.5em;padding-bottom:0.5em">
+                              <span>👇 </span
+                              ><span
+                                ><strong
+                                  >Download your activity pack here
+                                </strong></span
+                              ><span>👇</span>
+                            </p>
+                            <p
+                              class="node-paragraph"
+                              style="margin:0;padding:0;font-size:1em;padding-top:0.5em;padding-bottom:0.5em">
+                              <span
+                                ><a
+                                  href="https://drive.google.com/file/d/1NSobW1R5LC5ipjj-iiJVYROzVgd7oLBA/view?usp=drive_link"
+                                  rel="noopener noreferrer nofollow"
+                                  style="color:#0670DB;text-decoration-line:none;text-decoration:underline"
+                                  target="_blank"
+                                  >https://drive.google.com/file/d/1NSobW1R5LC5ipjj-iiJVYROzVgd7oLBA/view?usp=drive_link</a
+                                ></span
+                              >
+                            </p>
+                            <p
+                              style="margin:0;padding:0;font-size:1em;padding-top:0.5em;padding-bottom:0.5em">
+                              <span
+                                >If you enjoyed this printable, you’ll love what
+                                we’re creating at Amberwood House Press.
+                                Thoughtfully made stories and resources that
+                                invite children to slow down, wonder, and learn
+                                with heart.</span
+                              >
+                            </p>
+                            <p
+                              class="node-paragraph"
+                              style="margin:0;padding:0;font-size:1em;padding-top:0.5em;padding-bottom:0.5em">
+                              <span>Thank you for being here.</span><br /><span
+                                >We’re truly glad you found us.</span
+                              >
+                            </p>
+                            <p
+                              class="node-paragraph"
+                              style="margin:0;padding:0;font-size:1em;padding-top:0.5em;padding-bottom:0.5em">
+                              <span>Warmly,</span><br /><span
+                                ><strong>Amberwood House Press</strong></span
+                              ><br /><span>Amberwoodhousepress.com</span>
+                            </p>
+                            <p
+                              class="node-paragraph"
+                              style="margin:0;padding:0;font-size:1em;padding-top:0.5em;padding-bottom:0.5em">
+                              <br />
+                            </p>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <!--/$-->
+  </body>
+</html>
+
 });
 
 
